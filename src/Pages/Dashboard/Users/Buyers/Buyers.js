652cf4978c358d4d5da136291c876c5cfc67236d
minhajmia <minhajmia418@gmail.com>
2022-11-26 01:19:26 +0600

    buyer delete successful

--- src/Pages/Dashboard/Users/Buyers/Buyers.js
+++ src/Pages/Dashboard/Users/Buyers/Buyers.js
@@ -1,12 +1,28 @@
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import toast from "react-hot-toast";
 
 const Buyers = () => {
-  const { data: buyers = [] } = useQuery({
+  const { data: buyers = [], refetch } = useQuery({
     queryKey: ["/users/buyers"],
     queryFn: () =>
       fetch(`http://localhost:5000/users/buyers`).then((res) => res.json()),
   });
+  const handleDeleteBuyer = (id) => {
+    const proceed = window.confirm("Are you sure you want to delete?");
+    if (proceed) {
+      fetch(`http://localhost:5000/buyer/${id}`, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.deletedCount > 0) {
+            toast.success("Delete Successful");
+            refetch();
+          }
+        });
+    }
+  };
   return (
     <div>
       <h3 className="text-2xl font-bold">Total Buyers {buyers.length}</h3>
@@ -31,7 +47,12 @@ const Buyers = () => {
                     <td>{buyer.email}</td>
                     <td>{buyer.role}</td>
                     <td>
-                      <button className="btn btn-error">Delete</button>
+                      <button
+                        onClick={() => handleDeleteBuyer(buyer._id)}
+                        className="btn btn-error"
+                      >
+                        Delete
+                      </button>
                     </td>
                   </tr>
                 ))}
